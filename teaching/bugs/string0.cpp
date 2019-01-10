b60d47c2fb6d0441d5e192dbb2517e80d3e8ed4d
3k1m <39969119+3k1m@users.noreply.github.com>
2019-01-10 13:35:05 -0800

    Update string0.cpp

--- teaching/bugs/string0.cpp
+++ teaching/bugs/string0.cpp
@@ -1,10 +1,16 @@
 #include<string>
 #include<iostream>
 
+struct X{
+  std::string s;
+  // was a typo, meant to write s()!!!
+  X() : s(0) {}
+};
+
 int main(){
   // from what I can tell, it invokes a constructor expecting a const char* and a defaulted allocator
   // but treating the address 0 as a const char* is very bad...
-  std::string s(0);
-  std::cout << s << "...";
+  X x;
+  std::cout << x.s << "...";
   return 0;
 }
