8f9a7b2de3ce79faea0c1f22dd2d023f74480c2c
3k1m <39969119+3k1m@users.noreply.github.com>
2019-04-14 10:56:24 -0700

    Update copy_vs_move_efficiency.cpp

--- teaching/short_examples/copy_vs_move_efficiency.cpp
+++ teaching/short_examples/copy_vs_move_efficiency.cpp
@@ -41,16 +41,33 @@ int main(){
    	std::vector<No_Moves> no_moves(1000);
 	std::vector<Has_Moves> has_moves(1000);
     
+	// Example 1: copying and moving from this vector
+	
 	simple_timer::timer<'s',long double> tim; // timer object for seconds with long double precision
 	do_copies(no_moves);
-	auto time_copies = tim.tock(); // time with copy constructors
+	auto time_copies1 = tim.tock(); // time with copy constructors
 	
 	tim.tick();
 	do_moves(has_moves);
-	auto time_moves = tim.tock(); // time with move constructors
+	auto time_moves1 = tim.tock(); // time with move constructors
 	
-	std::cout << "Copy constructors: " << time_copies << '\n';
-	std::cout << "Move constructors: " << time_moves << '\n';
+	// Example 2: directly copying/moving from a string
+	
+	std::string s_to_copy(1000,'a');
+    	std::string s_to_move(1000,'a');
+    
+	tim.tick();
+	std::string copied = s_to_copy;
+	auto time_copies2 = tim.tock(); // time with copy constructors
+	
+	tim.tick();
+	std::string moved = std::move(s_to_move);
+	auto time_moves2 = tim.tock(); // time with move constructors
+	
+	std::cout << "Copy constructors example 1: " << time_copies1 << '\n';
+	std::cout << "Move constructors example 1: " << time_moves1 << '\n';
+	std::cout << "Copy constructors example 2: " << time_copies2 << '\n';
+	std::cout << "Move constructors example 2: " << time_moves2 << '\n';
 	
 	return 0;
 }
