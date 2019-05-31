4635988be23c6907e5384d701daa192adf2944ea
3k1m <39969119+3k1m@users.noreply.github.com>
2019-05-31 11:13:13 -0700

    Create template_nested_friend_examples_may31.cpp

---
+++ teaching/in_class_code/spring2019/template_nested_friend_examples_may31.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+namespace X{
+    
+  template<typename T>
+  struct Out{
+    class In{
+      int x;
+      friend void okay(const In&) {}  
+      friend void not_okay(const In&);
+    };  
+  };
+    
+  template<typename T>
+  void not_okay(const typename Out<T>::In& i) { i.x; }
+  
+  template<typename T>
+  void still_not_okay(const typename Out<T>::In& i) {}
+}
+
+int main()
+{
+  // make an inner class
+  X::Out<double>::In a;
+  okay(a);
+  //X::okay(a); // cannot be fully qualified
+
+  // friend not_okay is not a templated function
+  //X::not_okay<double>(a);    
+    
+  //X::still_not_okay(a); // cannot deduce type!
+  X::still_not_okay<double>(a); 
+
+
+  std::cout << "ran";
+}
