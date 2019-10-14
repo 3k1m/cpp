f7b7f5c4de2936bdb547548dd3cc86124e879202
3k1m <39969119+3k1m@users.noreply.github.com>
2019-10-14 11:07:19 -0700

    Create Accumulator_Main.cpp

---
+++ teaching/in_class_code/autumn2019/Accumulator_Main.cpp
@@ -0,0 +1,34 @@
+#include<iostream> // C++ standard get < >
+#include "Accumulator.h" // our own get " "
+
+using namespace std;
+
+
+int main() {
+
+
+	// common error
+	//Accumulator a();
+
+	Accumulator a; // will start at 0
+
+	Accumulator b(12); // starts at 0
+
+	// add 5 and 10 to both and b
+	a.add(5);
+	a.add(10);
+
+	b.add(5);
+	b.add(10);
+
+	// "add" and "get_total" are 
+	// member functions and as such
+	// are accessed with a .
+
+	cout << "a total: " << a.get_total() << '\n';
+	cout << "b total: " << b.get_total() << '\n';
+
+	cin.get();
+
+	return 0;
+}
