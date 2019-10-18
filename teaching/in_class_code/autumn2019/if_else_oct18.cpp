774437131467feed49ad5c7cb5cf10322ddbba9b
3k1m <39969119+3k1m@users.noreply.github.com>
2019-10-18 10:56:10 -0700

    Create if_else_oct18.cpp

---
+++ teaching/in_class_code/autumn2019/if_else_oct18.cpp
@@ -0,0 +1,41 @@
+// Example program
+#include <iostream>
+#include <string>
+
+int main()
+{
+  int x = 100;
+  //int x = 3;
+  
+  // || or
+  // && and
+  // ! not  
+  
+  if(true or false){
+    int x = 99; // x masks the x above
+    std::cout << x << '\n';
+  }
+  
+  std::cout << x << '\n';
+    
+    
+  /*if( true ) {
+   std::cout << "first";   
+  }
+  else if (true){
+   std::cout << "second";   
+  }
+  
+  std::cout << '\n';
+  
+  if( true ) {
+   std::cout << "first";   
+  }
+  else{
+      if (true){
+        std::cout << "second";
+      }
+  } */
+  
+  return 0;
+}
