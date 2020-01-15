0f786215819f585395e466b72d5b45e5d4039d65
3k1m <39969119+3k1m@users.noreply.github.com>
2020-01-15 09:55:56 -0800

    Create prefix_postfix_jan15.cpp

---
+++ teaching/in_class_code/winter2020/prefix_postfix_jan15.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x = 50;
+   
+   cout << "x: " << x << '\n';
+   ++x;
+   cout << "x: " << x << '\n';
+   cout << "++x: " << (++x) << '\n';
+   cout << "x: " << x << '\n';
+   cout << "x++: " << (x++) << '\n';
+   cout << "x: " << x << '\n';
+   
+   
+    return 0;
+}
