238a564035efa15fa4a0099b5f67bdb7b6ac5266
3k1m <39969119+3k1m@users.noreply.github.com>
2020-05-05 16:08:09 -0700

    Create templated_conversion.cpp

---
+++ teaching/bugs/templated_conversion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+template<typename T>
+struct Foo{
+    void bah() const { std::cout << "bah!"; }
+};
+
+template<typename T>
+struct Bar{
+    using foo_type = Foo<T>;
+    
+    // the line of could below should not be here!
+    // otherwise one has to call upon a templated 
+    // conversion operator
+    template<typename S>
+    explicit operator foo_type() const { return foo_type{}; }
+   
+};
+
+int main()
+{
+  Bar<double> b;
+  Foo<double> f = static_cast<Foo<double>>(b); // ERROR!
+  f.bah();
+}
