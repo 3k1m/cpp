1f6e61daf4b5a500d7d093dc3530e4b8d35ed737
3k1m <39969119+3k1m@users.noreply.github.com>
2022-04-14 21:24:49 -0700

    Create example.cpp

---
+++ teaching/FileSystem/example.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem; // abbreviate std::filesystem
+int main() {
+	try { // might throw
+		fs::create_directories("../foo/bar/bazzz");
+	}
+	catch (const fs::filesystem_error & E) { // catch the exceptions
+	}
+	try { // might throw
+		fs::create_directories("../foo/bar/to_remove");
+	}
+	catch (const fs::filesystem_error & E) { // catch the exceptions
+	}
+
+	std::ofstream("../foo/bar/to_remove/file.txt");
+	std::cout << fs::remove_all("../foo/bar/to_remove") << '\n'; // not empty!
+	std::ofstream("../foo/bar/file1.txt");
+	std::ofstream("../foo/bar/file2.txt");
+	std::ofstream("file3.txt");
+
+	if (fs::exists("../foo/bar/file3.txt")) { // file already exists?
+		fs::remove("../foo/bar/file3.txt");
+	}
+	fs::copy("file3.txt", "../foo/bar/"); // now safe to copy
+	if (fs::exists("../foo/bar/baz")) { // directory already exists?
+		fs::remove("../foo/bar/baz");
+	}
+	fs::rename("../foo/bar/bazzz", "../foo/bar/baz");
+	fs::directory_iterator iter("../foo/bar");
+	for (const auto& p : iter) { // each path
+		if (p.is_regular_file()) { // a file
+			std::cout << "file: " << p.path().string() << '\n';
+		}
+		else if (p.is_directory()) { // a directory
+			std::cout << "directory: " << p.path().string() << '\n';
+		}
+		else { // should not happen here
+			std::cout << "something else: " << p.path().string() << '\n';
+		}
+	}
+	return 0;
+}
