2bb9dfd54061559f613d52c4ec27501e575ba023
3k1m <39969119+3k1m@users.noreply.github.com>
2022-09-15 11:06:52 -0500

    Create sept15_1.cpp

---
+++ teaching/in_class_code/math3343_autumn2022/sept15_1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+int main(){
+ /* int N;
+
+  std::cout << "Enter a value for N: ";
+
+  std::cin >> N;
+
+  std::cout << "You entered " << N << '\n';
+*/
+
+/*  int x, y;
+
+  std::cout << "Enter two ints: ";
+
+  std::cin >> x >> y;
+
+  std::cout << "values are: " << x << ' ' << y << '\n';
+*/
+
+  int a;
+  double d;
+
+  int x;
+
+  std::cout << "Enter an int and a double: ";
+
+  std::cin >> a >> d >> x;
+
+  std::cout << "Values are: " << a << ' ' << d << ' ' << x << '\n';
+
+}
