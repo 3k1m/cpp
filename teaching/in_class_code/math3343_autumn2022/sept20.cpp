46150f6d39d7384822205f00609584fac834ff8c
3k1m <39969119+3k1m@users.noreply.github.com>
2022-09-20 11:11:42 -0500

    Create sept20.cpp

---
+++ teaching/in_class_code/math3343_autumn2022/sept20.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+
+int main(){
+  char c= std::cin.get();
+  std::cout << c << '\n';
+	
+	/*  int fav;
+  std::string answer;
+
+  std::cout << "Enter your favourite number: ";
+  std::cin >> fav;
+  std::cout << "Why is " << fav << " your favourite number? ";
+
+
+  std::cin.ignore();
+  std::getline(std::cin, answer);
+
+  std::cout << "You answer was: " << answer << '\n';
+*/
+}
+
+
